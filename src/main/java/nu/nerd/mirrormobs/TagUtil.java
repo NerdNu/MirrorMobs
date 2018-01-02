2d7da3182266d1feeebcaa74725c785fbffdb688
redwallhp <mcredwallhp@gmail.com>
2018-01-02 17:44:34 -0500

    Comment TagUtil

--- src/main/java/nu/nerd/mirrormobs/TagUtil.java
+++ src/main/java/nu/nerd/mirrormobs/TagUtil.java
@@ -4,6 +4,9 @@ package nu.nerd.mirrormobs;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
+/**
+ * Uses Scoreboard tags for persistent storage of metadata on entities.
+ */
 public class TagUtil {
 
 
@@ -11,17 +14,30 @@ public class TagUtil {
     private static final String IDPREFIX = "mirrormob.id:";
 
 
+    /**
+     * Mark an entity as a custom mob
+     * @param entity the entity to mark
+     * @param id a valid ID of a custom mob
+     */
     public static void tagCustomMob(LivingEntity entity, String id) {
         entity.addScoreboardTag(ISCUSTOM);
         entity.addScoreboardTag(TagUtil.getMobTagString(id));
     }
 
 
+    /**
+     * Determine if a mob is custom or not.
+     * This is faster than getTaggedMobId.
+     */
     public static boolean isEntityCustom(Entity entity) {
         return entity.getScoreboardTags().contains(ISCUSTOM);
     }
 
 
+    /**
+     * Determine the custom mob ID of a given entity.
+     * For performance, always check isEntityCustom() first.
+     */
     public static String getTaggedMobId(Entity entity) {
         for (String tag : entity.getScoreboardTags()) {
             if (tag.startsWith(IDPREFIX)) {
